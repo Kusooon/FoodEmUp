38e6e6e873f86f6cef77df7e1f8a36ca5caab235
Kusooon <karanyaphat.phu@gmail.com>
2023-07-29 16:36:29 +0700

    textmeshprogui

--- Assets/Scripts/SHMUP/Timer/TimerScript.cs
+++ Assets/Scripts/SHMUP/Timer/TimerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,7 @@ public class TimerScript : MonoBehaviour
     [SerializeField] private float TimeLeft;
     public bool TimerOn = false;
 
-    [SerializeField] private Text TimerTxt;
+    [SerializeField] private TextMeshProUGUI TimerTxt;
 
     void Start()
     {
